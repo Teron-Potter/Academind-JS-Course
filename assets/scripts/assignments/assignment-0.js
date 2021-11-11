361f4d337dac9dbe1cbe01aad972049b3a65df9b
Teron Potter <teronpotter@icloud.com>
2021-11-11 07:38:53 -0600

    Complete first assignment

---
+++ assets/scripts/assignments/assignment-0.js
@@ -0,0 +1,18 @@
+//Create two cariables: One that holds a fictional user input (a number of your choosing) and one that holds no value (e.g. "result").
+let UserInput = 64;
+let result;
+
+//Set the result variable to 18 plus the value stored in user input
+result = 18 + UserInput;
+
+//Add three additional lines of code where you change the result variable again
+result = result - 8;
+result = result * 4;
+result = result / 2;
+
+//Think about the value stored in the user input variable you also created - did that value change?
+  //no, UserInput is still 64
+
+//alert() the result and the user input variables (in two seperate alert() calls).
+alert(result);
+alert(UserInput);
